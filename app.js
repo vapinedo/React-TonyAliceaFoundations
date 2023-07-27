fcc80b170dd328ecac37321f2e652b16605baba9
Victor Pinedo <victorpinedomartinez@gmail.com>
2023-07-27 11:20:47 -0500

    add virtual DOM and logic to traversing

--- app.js
+++ app.js
@@ -1,21 +1,78 @@
-
-const countApp = {
-    getCount: () => {
-        const countElement = document.getElementById("count");
-        return Number(countElement.textContent);
-    },
-    
-    setCount: (val) => {
-        const countElement = document.getElementById("count");
-        countElement.textContent = val;
-    }
+let markup = {
+    type: "article",
+    children: [
+        {
+            type: "h2",
+            children: [
+                {
+                    type: "text",
+                    value: "Counter"
+                }
+            ]
+        },
+        {
+            type: "h3",
+            children: [
+                {
+                    type: "text",
+                    value: "This Works!"
+                }
+            ]
+        },
+        {
+            type: "p",
+            children: [
+                {
+                    type: "text",
+                    value: "Counter"
+                },
+                {
+                    type: "strong",
+                    children: [
+                        {
+                            type: "em",
+                            children: [
+                                {
+                                    type: "text",
+                                    value: " 1 "
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    type: "text",
+                    value: "times"
+                }
+            ]
+        },
+        {
+            type: "button",
+            children: [
+                {
+                    type: "text",
+                    value: "Click me"
+                }
+            ]
+        }
+    ]
 };
 
-function setCount() {
-    let count = countApp.getCount();
-    if (count >= 5) {
-        countApp.setCount(0);
-    } else {
-        countApp.setCount(count + 1);
+const main = document.getElementById("app");
+console.log(main);
+
+function addElements(pojoElement, parentDOMNode) {
+    let newDOMNode = pojoElement.type === "text"
+        ? document.createTextNode(pojoElement.value)
+        : document.createElement(pojoElement.type);
+
+    if (pojoElement.children) {
+        pojoElement.children.forEach((child) => {
+            addElements(child, newDOMNode); 
+        });
     }
-}
+    console.log(parentDOMNode);
+    parentDOMNode.appendChild(newDOMNode);
+};
+
+addElements(markup, main );
