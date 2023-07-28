ebc8555074b18e7ba8a0f3ad5726b743c9600136
Victor Pinedo <victorpinedomartinez@gmail.com>
2023-07-27 19:49:49 -0500

    add fake DOM Tree to traverse

--- app.js
+++ app.js
@@ -1,78 +1,12 @@
-let markup = {
-    type: "article",
-    children: [
-        {
-            type: "h2",
-            children: [
-                {
-                    type: "text",
-                    value: "Counter"
-                }
-            ]
-        },
-        {
-            type: "h3",
-            children: [
-                {
-                    type: "text",
-                    value: "This Works!"
-                }
-            ]
-        },
-        {
-            type: "p",
-            children: [
-                {
-                    type: "text",
-                    value: "Counter"
-                },
-                {
-                    type: "strong",
-                    children: [
-                        {
-                            type: "em",
-                            children: [
-                                {
-                                    type: "text",
-                                    value: " 1 "
-                                }
-                            ]
-                        }
-                    ]
-                },
-                {
-                    type: "text",
-                    value: "times"
-                }
-            ]
-        },
-        {
-            type: "button",
-            children: [
-                {
-                    type: "text",
-                    value: "Click me"
-                }
-            ]
-        }
-    ]
-};
+const rootNode = document.getElementById("app");
+const root = ReactDOM.createRoot(rootNode);
+root.render(React.createElement(App));
 
-const main = document.getElementById("app");
-console.log(main);
+console.log(React);
 
-function addElements(pojoElement, parentDOMNode) {
-    let newDOMNode = pojoElement.type === "text"
-        ? document.createTextNode(pojoElement.value)
-        : document.createElement(pojoElement.type);
+function App() {
+    console.log("Called App");
+    return React.createElement("button", null, "Click Me");
+}
 
-    if (pojoElement.children) {
-        pojoElement.children.forEach((child) => {
-            addElements(child, newDOMNode); 
-        });
-    }
-    console.log(parentDOMNode);
-    parentDOMNode.appendChild(newDOMNode);
-};
-
-addElements(markup, main );
+console.log(App()); 
