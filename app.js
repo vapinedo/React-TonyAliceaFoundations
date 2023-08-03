cd10cc4953388fbe12e18f727272e55d6a016709
Victor Pinedo <victorpinedomartinez@gmail.com>
2023-08-02 19:53:49 -0500

    JSX and Thinking In Elements

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
 const rootNode = document.getElementById("app");
 const root = ReactDOM.createRoot(rootNode);
+let counterName = "One";
 root.render(React.createElement(App));
 
 function App() {
+    const counterOne = <Counter name={counterName} />;
+    const counterTwo = <Counter2 name={counterName} />;
     return (
         <section>
             <h1>Counters</h1>
             <section>
-                <Counter />
+                {counterName === "One" ? counterOne : counterTwo}
             </section>
         </section>
-    );
+    ); 
 }
 
-function Counter() {
+function Counter({ name }) {
     return (
         <article>
-            <h2>Counter</h2>
-            <p>You clicked 1 times</p>
+            <h2>Counter {name}</h2>
+            <p>Counter 1: You clicked 1 times</p>
             <button className="button">Click me</button>
         </article>        
     );
 }
 
-rootNode.className = "root";
+function Counter2({ name }) {
+    return (
+        <article>
+            <h2>Counter {name}</h2>
+            <p>Counter 2: You clicked 1 times</p>
+            <button className="button">Click me</button>
+        </article>        
+    );
+}
+
+function rerender() {
+    console.log("Rerender...");
+    counterName = "Two";
+    root.render(React.createElement(App));   
+}
